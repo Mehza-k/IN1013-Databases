724ea3b5b043d364f8783eb00339e68889804522
Mehza-k <Mehza.Khan@city.ac.uk>
2024-11-19 10:26:03 +0000

    added hw 5

---
+++ HW 5/task1.sql
@@ -0,0 +1,23 @@
+-- 1
+
+SELECT bill_date, bill_total FROM restBill WHERE cust_name = 'Bob Crow';
+
+-- 2
+
+SELECT cust_name FROM restBill WHERE cust_name LIKE '%Smith' ORDER BY DESC;
+
+-- 3
+
+SELECT DISTINCT cust_name FROM restBill WHERE cust_name LIKE '% C%';
+
+-- 4
+
+SELECT first_name, surname FROM restStaff WHERE headwaiter IS NULL;
+
+--5
+
+SELECT * FROM restBill WHERE bill_date LIKE '1602%';
+
+-- 6
+
+SELECT DISTINCT bill_date FROM restBill WHERE bill_date like '15%';
