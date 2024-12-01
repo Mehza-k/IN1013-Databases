77c182b5c34687347cd5a2fef204ba88af97c859
Mehza-k <Mehza.Khan@city.ac.uk>
2024-12-01 20:12:31 +0000

    HW 6 updated

--- HW 6/task4.sql
+++ HW 6/task4.sql
@@ -1,10 +1,9 @@
--- 1 do again cose its worng
+-- 1
+
 SELECT T1.cust_name FROM restBill T1 WHERE T1.bill_total > 450.00 AND T1.waiter_no = 
 (SELECT DISTINCT T2.staff_no FROM restStaff T2 WHERE T2.headwaiter = 'Charles');
 
--- 2  2.A customer called Nerida has complained that a waiter was rude to her when 
---     she dined at the restaurant on the 11th January 2016. What is the name and
---     surname of the Headwaiter who will have to deal with the matter?
+-- 2 
 
 SELECT T1.first_name, T1.surname 
 FROM restStaff T1 
@@ -12,14 +11,16 @@ WHERE T1.staff_no =
 (SELECT T2.headwaiter FROM restStaff T2 WHERE T2.staff_no IN
 (SELECT T3.waiter_no FROM restBill T3  WHERE T3.bill_date = '160111' AND cust_name LIKE "Nerida %"));
 
--- 3 What are the names of customers with the smallest bill?
+-- 3
+
 SELECT T1.cust_name FROM restBill T1 WHERE T1.bill_total = 
 (SELECT MIN(T2.bill_total) FROM restBill T2);
 
--- 4  List the names of any waiters who have not taken any bills.
+-- 4  
+
 SELECT T1.first_name, T1.surname FROM restStaff T1 WHERE T1.headwaiter IS NOT NULL 
 AND T1.staff_no NOT IN (SELECT T2.waiter_no FROM restBill T2);
 
--- 5 5.	Which customers had the largest single bill? List the customer name, 
---the name and surname of headwaiters, and the room name where they were served on that occasion.
+-- 5 
+
 ?
--- HW 6/task5.sql
+++ HW 6/task5.sql
@@ -1,37 +1,40 @@
--- 1 1.	Which waiters have taken 2 or more bills on a single date? 
---List the waiter names and surnames, the dates and the number of bills they have taken.
+-- 1
+
 SELECT T1.first_name, T1.surname, T2.bill_date, COUNT(T2.bill_no) 
 FROM restStaff T1 INNER JOIN restBill T2 ON T1.staff_no = T2.waiter_no
 GROUP BY T1.first_name, T1.surname, T2.bill_date
 HAVING COUNT(T2.bill_no) > 1;
 
--- 2 2.	List the rooms with tables that can server more than 6 people and how many of such tables they have.
+-- 2
+
 SELECT T1.room_name, COUNT(T2.table_no) 
 FROM restRoom_management T1 INNER JOIN restRest_table T2 ON T1.room_name = T2.room_name
 WHERE T2.no_of_seats > 6
 GROUP BY T1.room_name;
 
---3 3.	List the names of the rooms and the total amount of bills in each room 
+--3
+
 SELECT T1.room_name, COUNT(T2.bill_no) 
 FROM restRest_table T1 INNER JOIN restBill T2 ON T1.table_no = T2.table_no
 GROUP BY T1.room_name;
 
--- 4  4.	List the headwaiter’s name and surname and the total bill amount their waiters have taken. 
--- Order the list by total bill amount, largest first.
+-- 4
+
 SELECT T1.first_name, T1.surname, SUM(T3.bill_total) 
 FROM restStaff T1 INNER JOIN restStaff T2 ON T2.headwaiter = T1.staff_no
 INNER JOIN restBill T3 ON T2.staff_no = T3.waiter_no
 GROUP BY T1.first_name, T1.surname
 ORDER BY SUM(T3.bill_total) DESC;
 
+-- 5
 
--- 5.	List any customers who have spent more than £400 on average.
 SELECT T1.cust_name, AVG(T1.bill_total) 
 FROM restBill T1
 GROUP BY T1.cust_name
 HAVING AVG(T1.bill_total) > 400.00;
 
--- 6.	Which waiters have taken 3 or more bills on a single date? List the waiter names, surnames, and the number of bills they have taken.
+-- 6
+
 SELECT T1.first_name, T1.Surname, COUNT(T2.bill_no) 
 FROM restStaff T1 INNER JOIN restBill T2 ON T1.staff_no = T2.waiter_no
 GROUP BY T1.first_name, T1.Surname
